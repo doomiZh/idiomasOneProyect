de90c063d467c5f31932b591eb65c251dcfecfa2
Leonardo2020-col <leonardopareja80@gmail.com>
2022-05-29 22:39:09 -0500

    avance idiomas

--- src/main/java/pe/com/pi/idiomasone/controllers/IdiomasController.java
+++ src/main/java/pe/com/pi/idiomasone/controllers/IdiomasController.java
@@ -1,5 +1,33 @@
 package pe.com.pi.idiomasone.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pe.com.pi.idiomasone.model.Idiomas;
+import pe.com.pi.idiomasone.service.IdiomasService;
+
+@Controller
+@RequestMapping("/idiomas")
 public class IdiomasController {
+	
+	@Autowired
+	private IdiomasService idiomasService;
+	
+	@GetMapping
+	public String listIdioma(Model idio) {
+		idio.addAttribute("listarIdioma", idiomasService.obtenerIdiomas());
+		return "idiomas/listar";
+	}
+	
+	@GetMapping("/nuevo")
+	public String IdiomaNuevo(Model idio) {
+		idio.addAttribute("idiomas", new Idiomas());
+		idio.addAttribute("listarIdioma", idiomasService.obtenerIdiomas());
+		return "idiomas/nuevo";
+	}
+	
 
 }
--- src/main/java/pe/com/pi/idiomasone/model/Idiomas.java
+++ src/main/java/pe/com/pi/idiomasone/model/Idiomas.java
@@ -1,5 +1,30 @@
 package pe.com.pi.idiomasone.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Idiomas {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nombre", length = 100, nullable = false)
+	private String nombre;
 
 }
