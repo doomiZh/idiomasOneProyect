a51039c0a11d150a1447d344b808bc74de332b0e
cmonago1 <91387247+cmonago1@users.noreply.github.com>
2022-05-31 16:17:36 -0500

    Update Usuarios.java

--- src/main/java/pe/com/pi/idiomasone/model/Usuarios.java
+++ src/main/java/pe/com/pi/idiomasone/model/Usuarios.java
@@ -1,5 +1,32 @@
 package pe.com.pi.idiomasone.model;
 
-public class Usuarios {
+@Data
+@Entity
+@Table(name="tb_usuario")
+public class Usuarios implements Serializable{
+  
+  private static final long serialVersionUID=1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_usuario;
+	
+	@NotEmpty
+	private String nombre;
+	
+	@NotEmpty
+	private String apellido;
+	
+	@NotEmpty
+	private String username;
+	
+	@NotEmpty
+	private String contrasena;
+	
+	@Email
+	private String correo;
+	
+	private int id_rol;
 
 }
