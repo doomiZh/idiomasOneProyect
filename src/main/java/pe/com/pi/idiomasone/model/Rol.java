0b25b6b28ea009314c149d6fbe6e0254ce7113d4
cmonago1 <91387247+cmonago1@users.noreply.github.com>
2022-05-31 16:20:19 -0500

    Create Rol.java

---
+++ src/main/java/pe/com/pi/idiomasone/model/Rol.java
@@ -0,0 +1,28 @@
+package pe.com.pi.idiomasone.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="tb_rol")
+public class Rol implements Serializable{	
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_rol;
+    
+    private String rol;
+
+}
