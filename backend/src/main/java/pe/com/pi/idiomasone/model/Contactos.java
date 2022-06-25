3d8d7071c192bb87492e7098f538d4edcf35916b
JulioAlfredo23 <106492695+JulioAlfredo23@users.noreply.github.com>
2022-06-24 20:18:00 -0500

    avance crud contactos - Julio
    avance crud contactos - Julio

--- backend/src/main/java/pe/com/pi/idiomasone/model/Contactos.java
+++ backend/src/main/java/pe/com/pi/idiomasone/model/Contactos.java
@@ -1,5 +1,110 @@
 package pe.com.pi.idiomasone.model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
 public class Contactos {
+
+   @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "Debe ingresar su nombre")
+	private String nombre;
+
+	@NotEmpty(message = "Debe ingresar su email")
+	@Email
+	private String email;
+
+	@NotBlank(message = "Debe ingresar su celular")
+	private String celular;
+
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	@NotNull(message = "Debe ingresar una fecha correcta")
+	private LocalDate fechaClase;
+
+	private LocalDateTime fechaRegistro;
+
+    public Contactos(Long id, String nombre, String email, String celular, LocalDate fechaClase, LocalDateTime fechaRegistro) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.celular = celular;
+        this.fechaClase = fechaClase;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+        
+        
+        
+    public Contactos() {
+    }
+
+     
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public LocalDate getFechaClase() {
+        return fechaClase;
+    }
+
+    public void setFechaClase(LocalDate fechaClase) {
+        this.fechaClase = fechaClase;
+    }
+
+    public LocalDateTime getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDateTime fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+	
+	
+	@PrePersist
+	public void asignarFechaRegistro() {
+		fechaRegistro = LocalDateTime.now();
+	}
 
 }
